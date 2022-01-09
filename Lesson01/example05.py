9ac77d91ba50ee968e93ae0c020671325663efad
YulAlKuznetsova <96434130+YulAlKuznetsova@users.noreply.github.com>
2022-01-10 01:48:27 +0300

    Update example05.py
    correction

--- Lesson01/example05.py
+++ Lesson01/example05.py
@@ -1,12 +1,12 @@
 revenue = int(input("Введите сумму выручки"))
 costs = int(input("Введите сумму издержек"))
 if revenue > costs:
-    print("Фирма работает в прибыль")
     profit = revenue - costs
-profitability = profit / revenue
-print("Рентабельность фирмы =", profitability)
-quantity = int(input("Введите количесвто сотрудников"))
-profit1 = profit / quantity
-print("Прибыль на одного сотрудника =", profit1)
+    print("Фирма работает в прибыль")
+    profitability = profit / revenue
+    print("Рентабельность фирмы =", profitability)
+    quantity = int(input("Введите количесвто сотрудников"))
+    profit1 = profit / quantity
+    print("Прибыль на одного сотрудника =", profit1)
 else:
-print("Фирма работает в убыток")
+    print("Фирма работает в убыток")
